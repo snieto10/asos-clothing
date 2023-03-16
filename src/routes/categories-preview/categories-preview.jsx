2ee721bc9ac0ee0f43d2b5c65bc7ea6223044c71
Sergio Nieto <snieto10@gmail.com>
2023-03-16 11:07:27 -0400

    Category Preview Component

--- src/routes/categories-preview/categories-preview.jsx
+++ src/routes/categories-preview/categories-preview.jsx
@@ -1,23 +1,24 @@
 import { useContext } from 'react';
 
 import { CategoriesContext } from '../../context/categories.context';
-import CategoryPreview from './../../components/category-preview.component.jsx/category-preview.component';
+import CategoryPreview from '../../components/category-preview.component.jsx/category-preview.component';
 
-import './shop.styles.scss';
-
-function Shop() {
+function CategoriesPreview() {
   const { categoriesMap } = useContext(CategoriesContext);
 
+  console.log(categoriesMap);
+
   return (
-    <div className='shop-container'>
+    <>
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
+
         return (
           <CategoryPreview key={title} title={title} products={products} />
         );
       })}
-    </div>
+    </>
   );
 }
 
-export default Shop;
+export default CategoriesPreview;
