8258e351b12ba5036cf389c72914570901fd6570
Sergio Nieto <snieto10@gmail.com>
2023-03-24 11:01:40 -0400

    publidhed

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,4 +1,5 @@
 import './category-item.styles.scss';
+import { Link } from 'react-router-dom';
 
 const CategoryItem = ({ category }) => {
   const { title, imageUrl } = category;
@@ -9,9 +10,12 @@ const CategoryItem = ({ category }) => {
         className='background-image'
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
+
       <div className='category-body-container'>
         <h2>{title}</h2>
-        <p>Shop Now</p>
+        <Link to={`shop/${title}`}>
+          <p>Shop Now</p>{' '}
+        </Link>
       </div>
     </div>
   );
